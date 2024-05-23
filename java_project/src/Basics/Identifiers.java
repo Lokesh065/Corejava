1a5db2ecca6ce1210cfc607f6a68c3fad86fa4f0
lokesh <lokesh@LAPTOP-B6SAJ78U>
2024-05-23 16:16:10 +0530

    Project submition

---
+++ java_project/src/Basics/Identifiers.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		
+		int $number = 20;
+		System.out.println("value of the number variable is :"+ $number);
+		
+		String studentName="Lokesh";
+		System.out.println("value is :"+ studentName);
+	}
+
+}
---
+++ java_project/src/Basics/PrimitiveDataTypes.java
@@ -0,0 +1,34 @@
+package Basics;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		byte byteMax = 127;
+		byte byteMin = -128;
+		
+		System.out.println("Min range of byte is  "+byteMin+" Max range of byte is "+byteMax );
+		
+		
+		short shortMax = 32767;
+		short shortMin = -32768;
+		System.out.println("Minshort range of byte is "+shortMin+" Maxshort range of byte is "+shortMax);
+		
+		
+		int maxInt = 234566789;
+		int minInt = -1234567890;
+		System.out.println("Minint range of byte is"+minInt+"Maxint range of byte is"+maxInt);
+		
+		long maxLong = 1234567832344446676L;
+		long minLong = -3456787967895536789L;
+		System.out.println("Minlong range of byte is "+minLong+" Maxlong range of byte is "+maxLong);
+		
+		float f=3234.45566777282f;
+		double d=3456.8356575682687873878f;
+		System.out.println("float value is" +f+ "double value is" +d);
+		
+		boolean flag=false;
+		System.out.println("boolean value is "+flag);
+		
+	}
+
+}
