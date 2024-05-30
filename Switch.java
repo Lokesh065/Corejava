5e9d4ab4ffb94dc5e484654127bfd18baf762a7e
Lokesh065 <lokeshlohitha764@gmail.com>
2024-05-30 17:52:34 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,33 @@
+package KeywordStatements;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		char x= 'l';
+		switch (x)
+		{
+		case 'l':
+		case 'L':
+			System.out.println(x+ "is a letter" );
+			break;
+		case 'd':
+		case 'D':
+			System.out.println(x+" is a digit");
+			break;
+		case 'w':
+		case 'W':
+			System.out.println(x+"is a white space");
+			break;
+		case 's':
+		case 'S':
+			System.out.println(x+"is a special symbol");
+			default:
+				System.out.println(x+" is other than letter, digit, space or special symbol ");
+				break;
+		
+		}
+
+	}
+
+}
